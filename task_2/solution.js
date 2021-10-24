e4cc852f959b5902ecefe0062aa5353d314e704d
Tatyana Shvetsova <daydreamer_92@mail.ru>
2021-10-24 14:14:28 +0300

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,6 +5,15 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
+    let shippingSum;
+    if (productsSum == 0) {
+        shippingSum = 0;
+    } else if (productsSum >= freeShippingMinSum) {
+        shippingSum = 0;   
+    } else if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    };
+
     // создайте переменную shippingSum
 
     // если productsSum равно 0,
@@ -28,6 +37,14 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
+    let discountSum;
+    if (productsSum >= discountMinSum) {
+        discountSum = discountPart*productsSum/100
+    }
+    else {
+        discountSum = 0
+    };
+
     // создайте переменную discountSum
 
     // если productsSum больше или равно discountMinSum,
@@ -45,14 +62,19 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
-    // создайте переменную totalSum
+    let totalSum = productsSum - discountSum;
 
+    // создайте переменную totalSum
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+   
+    totalSum = totalSum + shippingSum;
 
     // прибавьте к totalSum значение shippingSum
+    let freeShipping;
+    freeShipping = !(shippingSum);
 
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
